31f2b73b896decef5077d7dc6f085224b8da37f0
Gu Andres <andres@robotani.ca>
2025-04-24 13:46:10 -0600

    install

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '[Unit]
+Description=Ollama Service
+After=network-online.target
+
+[Service]
+ExecStart=/usr/local/bin/ollama serve
+User=ollama
+Group=ollama
+Restart=always
+RestartSec=3
+Environment="PATH=$PATH"
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/ollama.service
